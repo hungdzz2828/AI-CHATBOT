6c6ef11285b25c7d026f9f5fa92e2c25bc820b41
hungdzz2828 <vnboss54@gmail.com>
2026-03-12 14:32:26 +0700

    Refactor AI client and update token handling

--- bot.py
+++ bot.py
@@ -1,36 +1,45 @@
 import discord
 from google import genai
 
-DISCORD_TOKEN = "MTQ4MTUzNDI2Mzg3NzY5NzU2Ng.GWpibj.zGnFReld5Y5NAo_I-3AnOZfAz3FcU47H2JWT6Y"
-GEMINI_API_KEY = "API_KEY"
+# ===== TOKEN =====
+DISCORD_TOKEN = "MTQ4MTUzNDI2Mzg3NzY5NzU2Ng.GdyqkT.7QVXEXOKXEK7vfxSGn2nyzIXWcmmjcCMGri_pM"
+GEMINI_API_KEY = "AIzaSyDLqVnTQskYV-tsC07CMuVWPgfqYh6IT0E"
 
-ai = genai.Client(api_key=GEMINI_API_KEY)
+# ===== AI CLIENT =====
+client_ai = genai.Client(api_key=GEMINI_API_KEY)
 
+# ===== DISCORD =====
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = discord.Client(intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f"Bot online: {bot.user}")
 
+
 @bot.event
 async def on_message(message):
 
+    # bỏ qua tin nhắn của bot
     if message.author == bot.user:
         return
 
+    question = message.content
+
     try:
-        response = ai.models.generate_content(
+        response = client_ai.models.generate_content(
             model="gemini-2.5-flash",
-            contents=message.content
+            contents=question
         )
 
         await message.channel.send(response.text)
 
     except Exception as e:
+        await message.channel.send("⚠ Lỗi AI")
         print(e)
-        await message.channel.send("AI lỗi")
+
 
 bot.run(DISCORD_TOKEN)
